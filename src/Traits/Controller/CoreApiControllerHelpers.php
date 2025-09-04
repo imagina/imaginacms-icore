d4c844b5c79087f355c67e88ed23a9afd964485d
Cristian Marin <marin101998@gmail.com>
2025-09-04 15:52:15 -0500

    + fix | v12.x 04-09-25 change default value in params take and page

--- src/Traits/Controller/CoreApiControllerHelpers.php
+++ src/Traits/Controller/CoreApiControllerHelpers.php
@@ -18,8 +18,8 @@ trait CoreApiControllerHelpers
     {
         return (object)[
             'order' => $request->input('order'),
-            'page' => $request->input('page', 1),
-            'take' => $request->input('take', 12),
+            'page' => $request->input('page', null),
+            'take' => $request->input('take', null),
             'filter' => (object)(json_decode($request->input('filter', '[]'), true) ?? []),
             'include' => array_filter(explode(',', $request->input('include', '')), fn($val) => $val !== ''),
             'fields' => array_filter(explode(',', $request->input('fields', '')), fn($val) => $val !== ''),
